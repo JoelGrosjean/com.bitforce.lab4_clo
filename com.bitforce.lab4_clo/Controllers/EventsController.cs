430972e70c0d5709bd5a4f003b281c523f3ff57d
Joel Grosjean <grosjean@sheridancollege.ca>
2017-04-10 23:54:37 -0400

    add events nad service areas controllers

---
+++ com.bitforce.lab4_clo/Controllers/EventsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using com.bitforce.lab4_clo.Models;
+
+namespace com.bitforce.lab4_clo.Controllers
+{
+    public class EventsController : Controller
+    {
+        private CLOContext db = new CLOContext();
+
+        // GET: Events
+        public ActionResult Index()
+        {
+            // var events = db.Events.Include(@ => @.Activity).Include(@ => @.ServiceArea);
+            return View(db.Events.ToList());
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            ViewBag.Activity_Id = new SelectList(db.Activities, "Id", "Name");
+            ViewBag.ServiceAreaId = new SelectList(db.ServiceAreas, "Id", "Name");
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,StartTime,EndTime,ServiceAreaId,Completed,Activity_Id")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Events.Add(@event);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Activity_Id = new SelectList(db.Activities, "Id", "Name", @event.Activity_Id);
+            ViewBag.ServiceAreaId = new SelectList(db.ServiceAreas, "Id", "Name", @event.ServiceAreaId);
+            return View(@event);
+        }
+
+        // GET: Events/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Activity_Id = new SelectList(db.Activities, "Id", "Name", @event.Activity_Id);
+            ViewBag.ServiceAreaId = new SelectList(db.ServiceAreas, "Id", "Name", @event.ServiceAreaId);
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,StartTime,EndTime,ServiceAreaId,Completed,Activity_Id")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@event).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Activity_Id = new SelectList(db.Activities, "Id", "Name", @event.Activity_Id);
+            ViewBag.ServiceAreaId = new SelectList(db.ServiceAreas, "Id", "Name", @event.ServiceAreaId);
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Event @event = db.Events.Find(id);
+            db.Events.Remove(@event);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ com.bitforce.lab4_clo/Controllers/ServiceAreasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using com.bitforce.lab4_clo.Models;
+
+namespace com.bitforce.lab4_clo.Controllers
+{
+    public class ServiceAreasController : Controller
+    {
+        private CLOContext db = new CLOContext();
+
+        // GET: ServiceAreas
+        public ActionResult Index()
+        {
+            return View(db.ServiceAreas.ToList());
+        }
+
+        // GET: ServiceAreas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ServiceArea serviceArea = db.ServiceAreas.Find(id);
+            if (serviceArea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(serviceArea);
+        }
+
+        // GET: ServiceAreas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ServiceAreas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Address,Description")] ServiceArea serviceArea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ServiceAreas.Add(serviceArea);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(serviceArea);
+        }
+
+        // GET: ServiceAreas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ServiceArea serviceArea = db.ServiceAreas.Find(id);
+            if (serviceArea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(serviceArea);
+        }
+
+        // POST: ServiceAreas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Address,Description")] ServiceArea serviceArea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(serviceArea).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(serviceArea);
+        }
+
+        // GET: ServiceAreas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ServiceArea serviceArea = db.ServiceAreas.Find(id);
+            if (serviceArea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(serviceArea);
+        }
+
+        // POST: ServiceAreas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ServiceArea serviceArea = db.ServiceAreas.Find(id);
+            db.ServiceAreas.Remove(serviceArea);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
