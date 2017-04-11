8e81f2a8d4cd2b6d9437eea8a50b94f81b59d4c6
Joel Grosjean <grosjean@sheridancollege.ca>
2017-04-10 21:53:58 -0400

    added code for adding users and roles

--- com.bitforce.lab4_clo/Migrations/Configuration.cs
+++ com.bitforce.lab4_clo/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace com.bitforce.lab4_clo.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +17,39 @@ namespace com.bitforce.lab4_clo.Migrations
 
         protected override void Seed(com.bitforce.lab4_clo.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var store = new RoleStore<IdentityRole>(context);
+            var manager = new RoleManager<IdentityRole>(store);
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var role = new IdentityRole { Name = "Admin" };
+                manager.Create(role);
+            }
+
+            if (!context.Roles.Any(r => r.Name == "Staff"))
+            {
+                var role = new IdentityRole { Name = "Staff" };
+                manager.Create(role);
+            }
+
+
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            //  This method will be called after migrating to the latest version.
+            if (!(context.Users.Any(u => u.UserName == "admin@clo.email")))
+            {
+                var userToInsert = new ApplicationUser { UserName = "admin@clo.email", PhoneNumber = "9055551234" };
+                userManager.Create(userToInsert, "changeme!");
+                userManager.AddToRole(userToInsert.Id, "Admin");
+            }
+
+            if (!(context.Users.Any(u => u.UserName == "staff@clo.email")))
+            {
+
+                var userToInsert = new ApplicationUser { UserName = "staff@clo.email", PhoneNumber = "4165557894" };
+                userManager.Create(userToInsert, "changeme!");
+                userManager.AddToRole(userToInsert.Id, "Staff");
+            }
         }
     }
 }
